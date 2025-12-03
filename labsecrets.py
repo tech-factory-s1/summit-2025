60a02e3b893eeb235c90e25aca3972a0e8820660
johnpease-s1 <65245307+johnpease-s1@users.noreply.github.com>
2025-12-03 10:15:20 +0000

    Add files via upload

---
+++ labsecrets.py
@@ -0,0 +1,75 @@
+          NETLIFY_AUTH_TOKEN: ${{ secrets.Ih6Md9dnOBjQwyoDDh2iv7nECiHVySX7eaXO8iWy3tM }}
+
+          GOOGLE_CAPTCHA_KEY = '6Lrjv_b_jgnybWRwKSn2P6lop58PGZ_NfewZWnRT'
+
+          GOOGLE_API_KEY = 'AIzaSyBUPHAjZl3n8Eza66ka6B78iVyPteC5MgM' 
+
+          GITHUB_KEY = '88df97769ab3185f2c0b2a73fdae1b27d89409ca'
+
+          Azure Application Id = 08a6587c-5a5a-43ca-8c3b-f13f12bb1c26
+
+          Azure Tenant Id = 2ef92694-2c89-448d-ab1f-8db7a336d6c7
+
+          Azure Client Secret = u8o8Q~R6vVJ-fjnHSlkz3wFYXQDrSeRWvxw6sa6q
+
+          STRIPE_SECRET_KEY: ${{ secrets.sk_live_51Mz9AbCdEfGhIjKlMnOpQrStUvWxYz123456 }}
+
+          SLACK_BOT_TOKEN: ${{ secrets.xoxb-123456789012-1234567890123-AbCdEfGhIjKlMnOp }}
+
+          AWS_ACCESS_KEY_ID: ${{ secrets.AKIAIOSFODNN7EXAMPLEID }}
+
+          MAILCHIMP_API_KEY: ${{ secrets.1234567890abcdef1234567890abcdef-us19 }}
+
+          -----BEGIN RSA PRIVATE KEY-----
+          MIIFAKESSHPRIVATEKEYEXAMPLE
+          -----END RSA PRIVATE KEY-----
+
+          sk_test_51L53TeSBwzby5YcBooVe7xqrw9DrV7SWyW5WKKYGSFO4fNSoDyd167DZrmIW6lZE6pHUDrYe9zfqGnCHTQUKhnPc00eL5w5pxY
+
+          VIRUSTOTAL_API_KEY = e0d1c2b3a4958674655342312019283746556473829102938475665748392019
+
+          PAGERDUTY_API_KEY = y_1234567890AbCdEfGh
+
+          FLOAT_API_KEY: ${{ secrets.a1b2c3d4e5f678901234567890abcdef12345678 }}
+
+          ZOOM_API_JWT: eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJleGFtcGxlIiwiZXhwIjoxNDk2MjkxODZ9.AbCdEfGhIjKlMnOpQrStUvWxYz123456
+
+          PASSBASE_API_KEY: ${{ secrets.b2c3d4e5f678901234567890abcdef1234567890 }}
+          
+          DIGGERNAUT_API_KEY: ${{ secrets.d4e5f678901234567890abcdef1234567890abcd }}
+
+          LESSANNOYINGCRM_API_KEY = 'A1b2C3d4E5f6G7h8I9j0k1L2m3N4o5P6'
+
+          ONEPAGECRM_API_KEY = '1234567890abcdef1234567890abcdef12345678'
+        
+          PIPEDRIVE_API_KEY = 'a1b2c3d4e5f678901234567890abcdef12345678'
+
+          FRESHDESK_API_KEY = 'AbCdEfGhIjKlMnOpQrSt'
+          
+          ZENDESK_API_TOKEN = 'AbCdEfGhIjKlMnOpQrStUvWxYz1234567890AbCd'
+
+          INTERCOM_ACCESS_TOKEN = 'dG9rZW46MTIzNDU2Nzg5MA=='
+
+          NEW_RELIC_API_KEY = 'NRAK-A1b2C3d4E5f6G7h8I9j0k1L2m3N4'
+          
+          DATADOG_API_KEYS = 'a1b2c3d4e5f678901234567890abcdef'
+
+          SENTRY_TOKEN = 'a1b2c3d4e5f678901234567890abcdef1234567890abcdef1234567890abcdef'
+          
+          CIRCLECI_TOKEN = 'a1b2c3d4e5f678901234567890abcdef12345678'
+
+          TRAVISCI_API_TOKEN = 'AbCdEfGhIjKlMnOpQrStUv'
+
+          BITLY_ACCESS_TOKEN = '40a9b8c7d6e5f432109876543210abcdef123456'
+
+          CLOUDINARY_BASIC_AUTH = 'cloudinary://123456789012345:AbCdEfGhIjKlMnOpQrStUvWxYz@cloudname'
+
+          PIXABAY_API_KEY = '1234567-abcdef1234567890abcdef123'
+
+          UNSPLASH_API_KEY = 'AbCdEfGhIjKlMnOpQrStUvWxYz1234567890AbCdEf'
+          
+          CONTENTFUL_PERSONAL_ACCESS_TOKEN = 'CFPAT-AbCdEfGhIjKlMnOpQrStUvWxYz1234567890AbCd'
+          
+          RAPID_API_KEY: ${{ secrets.a1b2c3d4e5f678901234567890abcdef12345678 }}
+
+          ALGOLIA_ADMIN_API_KEY: ${{ secrets.1234567890abcdef1234567890abcdef }}
